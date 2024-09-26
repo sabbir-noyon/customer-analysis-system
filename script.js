99aaacd9a7a66ad40267b0c1181f6dc1713c06ad
Sabbir-Noyon <sabbir.noyon.001@gmail.com>
2024-09-26 20:57:40 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const ctx = document.getElementById('pieChart').getContext('2d');
+
+const pieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }
+});
